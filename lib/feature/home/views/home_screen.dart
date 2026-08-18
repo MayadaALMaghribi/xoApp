c2c48b9e9a0e16f016f91951cbd8ee386af611d7
MayadaALMaghribi <latifa293h@gmail.com>
2026-08-19 01:30:25 +0300

    test are not finish

--- lib/feature/home/views/home_screen.dart
+++ lib/feature/home/views/home_screen.dart
@@ -1,12 +1,19 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:xoapp/core/constants/app_color.dart';
 import 'package:xoapp/core/constants/app_text_style.dart';
 import 'package:xoapp/core/widget/custom_scaffold_widget.dart';
 import 'package:xoapp/feature/home/widgets/xo_button_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffoldWidget(
@@ -24,6 +31,117 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  onPlayerClick(int index) {
+    board[index] = board[index].isEmpty ? "X" : "O";
+    log("done");
+    setState(() {});
+  }
+
+  Expanded buildBoard() => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        color: AppColor.white,
+        borderRadius: BorderRadius.circular(44),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    XoButtonWidget(
+                      symbol: board[0],
+                      onTap: () => onPlayerClick(0),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[1],
+                      onTap: () => onPlayerClick(1),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[2],
+                      onTap: () => onPlayerClick(2),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    XoButtonWidget(
+                      symbol: board[3],
+                      onTap: () => onPlayerClick(3),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[4],
+                      onTap: () => onPlayerClick(4),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[5],
+                      onTap: () => onPlayerClick(5),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    XoButtonWidget(
+                      symbol: board[6],
+                      onTap: () => onPlayerClick(6),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[7],
+                      onTap: () => onPlayerClick(7),
+                    ),
+                    XoButtonWidget(
+                      symbol: board[8],
+                      onTap: () => onPlayerClick(8),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Divider(
+                thickness: 1,
+                color: AppColor.black,
+                endIndent: 15,
+                indent: 15,
+              ),
+              Divider(
+                thickness: 1,
+                color: AppColor.black,
+                endIndent: 15,
+                indent: 15,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              VerticalDivider(
+                thickness: 1,
+                color: AppColor.black,
+                endIndent: 15,
+                indent: 15,
+              ),
+              VerticalDivider(
+                thickness: 1,
+                color: AppColor.black,
+                endIndent: 15,
+                indent: 15,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
 
 List<String> board = ["", "X", "O", "", "X", "", "", "", ""];
@@ -44,82 +162,4 @@ player() => Text(
   "Player 1’s Turn",
   style: AppTextStyle.white36Bold,
   textAlign: TextAlign.center,
-);
-
-Expanded buildBoard() => Expanded(
-  child: Container(
-    decoration: BoxDecoration(
-      color: AppColor.white,
-      borderRadius: BorderRadius.circular(44),
-    ),
-    child: Stack(
-      children: [
-        Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  XoButtonWidget(symbol: board[0], onTap: () {}),
-                  XoButtonWidget(symbol: "X", onTap: () {}),
-                  XoButtonWidget(symbol: "O", onTap: () {}),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  XoButtonWidget(symbol: "", onTap: () {}),
-                  XoButtonWidget(symbol: "X", onTap: () {}),
-                  XoButtonWidget(symbol: "O", onTap: () {}),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  XoButtonWidget(symbol: "", onTap: () {}),
-                  XoButtonWidget(symbol: "X", onTap: () {}),
-                  XoButtonWidget(symbol: "O", onTap: () {}),
-                ],
-              ),
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Divider(
-              thickness: 1,
-              color: AppColor.black,
-              endIndent: 15,
-              indent: 15,
-            ),
-            Divider(
-              thickness: 1,
-              color: AppColor.black,
-              endIndent: 15,
-              indent: 15,
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            VerticalDivider(
-              thickness: 1,
-              color: AppColor.black,
-              endIndent: 15,
-              indent: 15,
-            ),
-            VerticalDivider(
-              thickness: 1,
-              color: AppColor.black,
-              endIndent: 15,
-              indent: 15,
-            ),
-          ],
-        ),
-      ],
-    ),
-  ),
 );
